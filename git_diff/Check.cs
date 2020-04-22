693323e7cad789a26854dcb79d911d84922bf8ec
KaoticKas <kacperhajda16@gmail.com>
2020-04-22 13:40:32 +0100

    Assigment 3 changes
    just some changes to the code

--- git_diff/Check.cs
+++ git_diff/Check.cs
@@ -3,6 +3,7 @@
 public class Check
 {
 	bool isDifferent;
+	int counter;
 	//a boolean that wil lbe used to check if there is a difference in files or not
 	public Check()
 	{
@@ -11,36 +12,37 @@ public class Check
 		string[] listA = readObj.file1.ToArray();
 		string[] listB = readObj.file2.ToArray();
 		//converts the lists into arrays to be read into
-		if (listA.Length > listB.Length || listB.Length > listA.Length)
+		for (int i = 0; i < listA.Length; i++)
 		{
-			isDifferent = true;
-			//if the two arrays have different lengths they will be different so this is a condition that checks if either is larger than the other by using an or logic.
-			//if they are they set the bool to true.
-		}
-		else
-		{
-			for (int i = 0; i < listA.Length; i++)
-			{
+			counter++;
 				//for loop that goes through the entire array using a index i
-				if (listA[i] != listB[i])
-				{
-					isDifferent = true;
-					break;
+			if (listA[i] != listB[i])
+			{
+				isDifferent = true;
+				break;
 					//if the array indexes for both lists are not the same it will set the bool to true and break out of the loop.
-				}
-				else
-				{
-					isDifferent = false;
-					//if the lists are the same then they are not different so the bool is set to false
-				}		
 			}
+			else
+			{
+				isDifferent = false;
+					//if the lists are the same then they are not different so the bool is set to false
+			}		
 		}
+
 		if (isDifferent == true)
 		{
 			//if statement that checks the state of the bool to output the appropiate message.
 			Console.ForegroundColor = ConsoleColor.Red;
-			Console.WriteLine("a.txt and b.txt are different");
+			Console.WriteLine("a.txt and b.txt are different at Line:" + counter.ToString());
 			Console.ForegroundColor = ConsoleColor.White;
+
+			Console.WriteLine("Line" + counter.ToString() + ":");
+
+			Console.Write(listA[counter].ToString());
+			Console.WriteLine(listB[counter].ToString());
+
+
+
 			//if the files are different it will change the text colour and alert the user they are different.
 		}
 		else
